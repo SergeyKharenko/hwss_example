d6b8e39c3f55cad52e1cebef4c31e3f281c66c3c
SergeyKharenko <u201810610@hust.edu.cn>
2024-10-10 03:04:21 +0800

    In development

--- components/hwss/eth/io/hwss_io_spi_pro.c
+++ components/hwss/eth/io/hwss_io_spi_pro.c
@@ -17,15 +17,15 @@ static inline void SPI_PRO_DELAY_TICKS(uint32_t ticks){
     while(ticks--);
 };
 
-esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_setup(spi_device_handle_t handle, spi_transaction_t *trans_desc){
+static esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_setup(hwss_io_spi_pro_t *io_spi_pro, spi_transaction_t *trans_desc){
     esp_err_t ret;
-    ret = check_trans_valid(handle, trans_desc);
+    ret = check_trans_valid(io_spi_pro->hdl, trans_desc);
     if (ret != ESP_OK) {
         return ret;
     }
-    SPI_CHECK(!spi_bus_device_is_polling(handle), "Cannot send polling transaction while the previous polling transaction is not terminated.", ESP_ERR_INVALID_STATE);
+    SPI_CHECK(!spi_bus_device_is_polling(io_spi_pro->hdl), "Cannot send polling transaction while the previous polling transaction is not terminated.", ESP_ERR_INVALID_STATE);
 
-    spi_host_t *host = handle->host;
+    spi_host_t *host = io_spi_pro->hdl->host;
     spi_trans_priv_t priv_polling_trans = { .trans = trans_desc, };
     ret = setup_priv_desc(host, &priv_polling_trans);
     if (ret != ESP_OK) {
@@ -35,7 +35,7 @@ esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_setup(spi_device_handle_t handle, 
     /* If device_acquiring_lock is set to handle, it means that the user has already
      * acquired the bus thanks to the function `spi_device_acquire_bus()`.
      * In that case, we don't need to take the lock again. */
-    if (host->device_acquiring_lock != handle) {
+    if (host->device_acquiring_lock != io_spi_pro->hdl) {
         /* The user cannot ask for the CS to keep active has the bus is not locked/acquired. */
         if ((trans_desc->flags & SPI_TRANS_CS_KEEP_ACTIVE) != 0) {
             ret = ESP_ERR_INVALID_ARG;
@@ -58,9 +58,7 @@ esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_setup(spi_device_handle_t handle, 
 
     ESP_LOGV(SPI_TAG, "polling trans");
 
-    spi_device_t *dev=handle;
-    spi_hal_context_t *hal = &(dev->host->hal);
-    spi_hal_dev_config_t *hal_dev = &(dev->hal_dev);
+    spi_hal_dev_config_t *hal_dev = &(((spi_dev_t *)io_spi_pro->dev)->hal_dev);
 
     dev->host->cur_cs = dev->id;
 
@@ -75,23 +73,20 @@ esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_setup(spi_device_handle_t handle, 
     return ESP_OK;
 }
 
-static inline void hwss_io_spi_pro_update_bitlen(spi_device_handle_t handle, size_t bitlen){
-    spi_device_t *dev=handle;
-    spi_dev_t *hw = dev->host->hal.hw;
-    dev->host->hal.trans_config.tx_bitlen=bitlen;
-    dev->host->hal.trans_config.rx_bitlen=bitlen;
-    spi_ll_set_mosi_bitlen(hw, bitlen);
-    spi_ll_set_miso_bitlen(hw, bitlen);
+static inline void hwss_io_spi_pro_update_bitlen(hwss_io_spi_pro_t *io_spi_pro, size_t bitlen){
+    ((spi_hal_context_t *)io_spi_pro->hal)->trans_config.tx_bitlen=bitlen;
+    ((spi_hal_context_t *)io_spi_pro->hal)->trans_config.rx_bitlen=bitlen;
+    spi_ll_set_mosi_bitlen(io_spi_pro->hw, bitlen);
+    spi_ll_set_miso_bitlen(io_spi_pro->hw, bitlen);
 }
 
-static inline void hwss_io_spi_pro_prepare_data(spi_device_handle_t handle, uint8_t *tx, uint8_t *rx){
-    spi_device_t *dev=handle;
-    dev->host->hal.trans_config.rcv_buffer=rx;
-    dev->host->hal.trans_config.send_buffer=tx;
-    s_spi_prepare_data(dev,&(dev->host->hal.trans_config));
+static inline void hwss_io_spi_pro_prepare_data(hwss_io_spi_pro_t *io_spi_pro, uint8_t *tx, uint8_t *rx){
+    ((spi_device_t *)io_spi_pro->dev)->host->hal.trans_config.rcv_buffer=rx;
+    ((spi_device_t *)io_spi_pro->dev)->host->hal.trans_config.send_buffer=tx;
+    s_spi_prepare_data(io_spi_pro->dev,&(dev->host->hal.trans_config));
 }
 
-esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_write_head(hwss_io_spi_pro_t *io_spi_pro, uint16_t cmd, uint64_t addr, hwss_io_spi_pro_gap_t gap){
+static esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_write_head(hwss_io_spi_pro_t *io_spi_pro, uint16_t cmd, uint64_t addr, hwss_io_spi_pro_gap_t gap){
     esp_err_t ret=ESP_OK;
 
     uint8_t bias=0;
@@ -130,13 +125,16 @@ esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_write_head(hwss_io_spi_pro_t *io_s
     spi_hal_context_t *hal = &(io_spi_pro->hdl->host->hal);
     if(gap&HWSS_IO_SPI_PRO_GAP_HH){
         hwss_io_spi_pro_update_bitlen(io_spi_pro->hdl,8);
-        for(uint8_t i=0;i<bias;i++){
+        for(uint8_t i=0;i<bias-1;i++){
             hwss_io_spi_pro_prepare_data(io_spi_pro->hdl,io_spi_pro->cache+i,NULL);
             spi_hal_user_start(hal);
             while (!spi_hal_usr_is_done(hal));
             if(io_spi_pro->delay_hh)
                 SPI_PRO_DELAY_TICKS(io_spi_pro->delay_hh);
         }
+        hwss_io_spi_pro_prepare_data(io_spi_pro->hdl,io_spi_pro->cache+bias-1,NULL);
+        spi_hal_user_start(hal);
+        while (!spi_hal_usr_is_done(hal));
     }
     else{
         hwss_io_spi_pro_update_bitlen(io_spi_pro->hdl,bias<<3);
@@ -149,14 +147,26 @@ err:
     return ret;
 }
 
-esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_cache_write_data(hwss_io_spi_pro_t *io_spi_pro, const uint8_t *data, size_t len, hwss_io_spi_pro_gap_t gap){
+static esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_cache_write_data(hwss_io_spi_pro_t *io_spi_pro, const uint8_t *data, 
+                                                                            size_t len, hwss_io_spi_pro_gap_t gap){
     spi_hal_context_t *hal = &(io_spi_pro->hdl->host->hal);
     spi_dev_t *hw = hal->hw;
     
     memcpy(io_spi_pro->cache,data,len);
 
     if(gap&HWSS_IO_SPI_PRO_GAP_DD){
-
+        hwss_io_spi_pro_update_bitlen(io_spi_pro->hdl,8);
+        for(size_t i=0;i<len-1;i++){
+            hwss_io_spi_pro_prepare_data(io_spi_pro->hdl,io_spi_pro->cache+i,NULL);
+            spi_hal_user_start(hal);
+            while (!spi_hal_usr_is_done(hal));
+            if(io_spi_pro->delay_dd)
+                SPI_PRO_DELAY_TICKS(io_spi_pro->delay_dd);
+        }
+        spi_ll_master_keep_cs(hw,0);
+        hwss_io_spi_pro_prepare_data(io_spi_pro->hdl,io_spi_pro->cache+len-1,NULL);
+        spi_hal_user_start(hal);
+        while (!spi_hal_usr_is_done(hal));
     }
     else{
         spi_ll_master_keep_cs(hw,0);
@@ -185,5 +195,31 @@ esp_err_t SPI_MASTER_ISR_ATTR hwss_io_spi_pro_cache_read_data(hwss_io_spi_pro_t 
         }
     }
 
+    return ret;
+}
+
+esp_err_t hwss_io_spi_pro_init(hwss_io_t *io){
+    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+
+    io_spi_pro->cache=hwss_cache_dma_alloc(HWSS_IO_CACHE_SIZE);
+    io_spi_pro->cmd_bytes=io_spi_pro->spi_config.command_bits>>8;
+    io_spi_pro->addr_bytes=io_spi_pro->spi_config.address_bits>>8;
+    
+    
+    ESP_RETURN_ON_ERROR(spi_bus_add_device(io_spi_pro->spi_host_id,&(io_spi_pro->spi_config),&(io_spi_pro->hdl)),
+                        TAG,"fail to init spi_pro");
+    io_spi_pro->dev=&(io_spi_pro->hdl);
+    io_spi_pro->hal=&(io_spi_pro->hdl->host->hal);
+    io_spi_pro->hw=((spi_hal_context_t *)io_spi_pro->hal)->hw;
+
+    return ESP_OK;
+}
+
+esp_err_t hwss_io_spi_pro_deinit(hwss_io_t *io){
+    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+
+    esp_err_t ret=spi_bus_remove_device(io_spi_pro->hdl);
+    if(io_spi_pro->cache)
+        free(io_spi_pro->cache);
     return ret;
 }
--- components/hwss/eth/io/hwss_io_spi_pro.h
+++ components/hwss/eth/io/hwss_io_spi_pro.h
@@ -35,6 +35,10 @@ typedef struct{
     uint8_t             cmd_bytes;
     uint8_t             addr_bytes;
 
+    void                *dev;
+    void                *hal;
+    void                *hw;
+
     uint32_t             delay_hh;
     uint32_t             delay_hd;
     uint32_t             delay_dd;
@@ -43,8 +47,8 @@ typedef struct{
 void hwss_io_spi_pro_inter_init(hwss_io_spi_pro_t *io_spi_pro);
 
 esp_err_t hwss_io_spi_pro_write_head(hwss_io_spi_pro_t *io_spi_pro, uint16_t cmd, uint64_t addr, hwss_io_spi_pro_gap_t gap);
-esp_err_t hwss_io_spi_pro_write_data(hwss_io_spi_pro_t *io_spi_pro, const uint8_t *data, size_t len, hwss_io_spi_pro_gap_t gap);
-esp_err_t hwss_io_spi_pro_read_data(hwss_io_spi_pro_t *io_spi_pro, uint8_t *data, size_t len, hwss_io_spi_pro_gap_t gap);
+esp_err_t hwss_io_spi_pro_cache_write_data(hwss_io_spi_pro_t *io_spi_pro, const uint8_t *data, size_t len, hwss_io_spi_pro_gap_t gap);
+esp_err_t hwss_io_spi_pro_cache_read_data(hwss_io_spi_pro_t *io_spi_pro, uint8_t *data, size_t len, hwss_io_spi_pro_gap_t gap);
 
 static inline bool HWSS_IO_SPI_PRO_LOCK(hwss_io_spi_pro_t *io){
     return (xSemaphoreTake(io->lock,pdMS_TO_TICKS(HWSS_IO_LOCK_TIMEOUT_MS)) == pdTRUE);
@@ -54,109 +58,94 @@ static inline bool HWSS_IO_SPI_PRO_UNLOCK(hwss_io_spi_pro_t *io){
     return (xSemaphoreGive(io->lock) == pdTRUE);
 }
 
-static inline esp_err_t hwss_io_spi_pro_init(hwss_io_t *io){
-    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+esp_err_t hwss_io_spi_pro_init(hwss_io_t *io);
 
-    io_spi_pro->cache=hwss_cache_dma_alloc(HWSS_IO_CACHE_SIZE);
-    io_spi_pro->cmd_bytes=io_spi_pro->spi_config.command_bits>>8;
-    io_spi_pro->addr_bytes=io_spi_pro->spi_config.address_bits>>8;
-    
-    return spi_bus_add_device(io_spi_pro->spi_host_id,&(io_spi_pro->spi_config),&(io_spi_pro->hdl));
-}
+esp_err_t hwss_io_spi_pro_deinit(hwss_io_t *io);
 
-static inline esp_err_t hwss_io_spi_pro_deinit(hwss_io_t *io){
-    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+// static inline esp_err_t hwss_io_spi_pro_cache_write(hwss_io_t *io, uint16_t cmd, uint64_t addr, const uint8_t *data, size_t len, 
+//                                                     hwss_io_spi_pro_gap_t gap){
+//     esp_err_t ret=ESP_OK;
+//     hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
 
-    esp_err_t ret=spi_bus_remove_device(io_spi_pro->hdl);
-    if(io_spi_pro->cache)
-        free(io_spi_pro->cache);
-    return ret;
-}
+//     if(len>HWSS_IO_CACHE_SIZE)
+//         return ESP_ERR_INVALID_SIZE;
 
-static inline esp_err_t hwss_io_spi_pro_cache_write(hwss_io_t *io, uint16_t cmd, uint64_t addr, const uint8_t *data, size_t len, 
-                                                    hwss_io_spi_pro_gap_t gap){
-    esp_err_t ret=ESP_OK;
-    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+//     if(gap==HWSS_IO_SPI_PRO_NOGAP){
+//         spi_transaction_t trans={
+//             .addr=addr,
+//             .cmd=cmd,
+//             .tx_buffer=io_spi_pro->cache,
+//             .rx_buffer=NULL,
+//             .length=(len<<3)
+//         };
 
-    if(len>HWSS_IO_CACHE_SIZE)
-        return ESP_ERR_INVALID_SIZE;
+//         if(!HWSS_IO_SPI_PRO_LOCK(io_spi_pro))
+//             return ESP_ERR_TIMEOUT;
+//         memcpy(io_spi_pro->cache,data,len);
+//         ret=spi_device_polling_transmit(io_spi_pro->hdl,&trans);
+//         HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
+//     }
+//     else{
+//         // spi_device_t *dev=io_spi_pro->hdl;
 
-    if(gap==HWSS_IO_SPI_PRO_NOGAP){
-        spi_transaction_t trans={
-            .addr=addr,
-            .cmd=cmd,
-            .tx_buffer=io_spi_pro->cache,
-            .rx_buffer=NULL,
-            .length=(len<<3)
-        };
+//         // spi_device_acquire_bus(dev,portMAX_DELAY);
+//         // HWSS_IO_SPI_PRO_LOCK(io_spi_pro);
 
-        if(!HWSS_IO_SPI_PRO_LOCK(io_spi_pro))
-            return ESP_ERR_TIMEOUT;
-        memcpy(io_spi_pro->cache,data,len);
-        ret=spi_device_polling_transmit(io_spi_pro->hdl,&trans);
-        HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
-    }
-    else{
-        // spi_device_t *dev=io_spi_pro->hdl;
+//         // host->polling = true;
 
-        // spi_device_acquire_bus(dev,portMAX_DELAY);
-        // HWSS_IO_SPI_PRO_LOCK(io_spi_pro);
+//         // ESP_LOGV(SPI_TAG, "polling trans");
+//         // dev->host->cur_cs = dev->id;
 
-        // host->polling = true;
-
-        // ESP_LOGV(SPI_TAG, "polling trans");
-        // dev->host->cur_cs = dev->id;
-
-        // if(gap&HWSS_IO_SPI_PRO_GAP_HH){
+//         // if(gap&HWSS_IO_SPI_PRO_GAP_HH){
             
-        // }
-        // else{
+//         // }
+//         // else{
 
-        // }
+//         // }
 
-        // if(gap&HWSS_IO_SPI_PRO_GAP_HD)
-        //     esp_rom_delay_us(io_spi_pro->delay_hd);
+//         // if(gap&HWSS_IO_SPI_PRO_GAP_HD)
+//         //     esp_rom_delay_us(io_spi_pro->delay_hd);
 
         
-        // if(gap&HWSS_IO_SPI_PRO_GAP_DD){
+//         // if(gap&HWSS_IO_SPI_PRO_GAP_DD){
             
 
-        // }
-        // else{
+//         // }
+//         // else{
             
             
-        // }
+//         // }
 
-        // HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
-        // spi_device_release_bus(dev);
-    }
-    return ret;
-}
+//         // HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
+//         // spi_device_release_bus(dev);
+//     }
+//     return ret;
+// }
 
-static inline esp_err_t hwss_io_spi_pro_cache_read(hwss_io_t *io, uint16_t cmd, uint64_t addr, uint8_t *data, size_t len,
-                                                    hwss_io_spi_pro_gap_t gap){
-    esp_err_t ret=ESP_OK;
-    hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
+// static inline esp_err_t hwss_io_spi_pro_cache_read(hwss_io_t *io, uint16_t cmd, uint64_t addr, uint8_t *data, size_t len,
+//                                                     hwss_io_spi_pro_gap_t gap){
+//     esp_err_t ret=ESP_OK;
+//     hwss_io_spi_pro_t *io_spi_pro=__containerof(io,hwss_io_spi_pro_t,super);
 
-    if(len>HWSS_IO_CACHE_SIZE)
-        return ESP_ERR_INVALID_SIZE;
+//     if(len>HWSS_IO_CACHE_SIZE)
+//         return ESP_ERR_INVALID_SIZE;
 
-    spi_transaction_t trans={
-        .addr=addr,
-        .cmd=cmd,
-        .tx_buffer=NULL,
-        .rx_buffer=io_spi_pro->cache,
-        .length=(len<<3)
-    };
+//     spi_transaction_t trans={
+//         .addr=addr,
+//         .cmd=cmd,
+//         .tx_buffer=NULL,
+//         .rx_buffer=io_spi_pro->cache,
+//         .length=(len<<3)
+//     };
 
-    if(!HWSS_IO_SPI_PRO_LOCK(io_spi_pro))
-        return ESP_ERR_TIMEOUT;
-    ret=spi_device_polling_transmit(io_spi_pro->hdl,&trans);
-    memcpy(data,io_spi_pro->cache,len);
-    HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
+//     if(!HWSS_IO_SPI_PRO_LOCK(io_spi_pro))
+//         return ESP_ERR_TIMEOUT;
+//     ret=spi_device_polling_transmit(io_spi_pro->hdl,&trans);
+//     memcpy(data,io_spi_pro->cache,len);
+//     HWSS_IO_SPI_PRO_UNLOCK(io_spi_pro);
 
-    return ret;
-}
+//     return ret;
+// }
 
 static inline hwss_io_spi_pro_t *hwss_io_new_spi_pro(const hwss_io_spi_pro_config_t* config){
     hwss_io_spi_pro_t *io_spi_pro=NULL;
